9fd3250003da82ea38685e9138235893e793aa8e
AndrewLester <alester220@gmail.com>
2018-05-21 18:45:25 -0400

    Add fieldnames variable to AioDictReader and encoding setting

--- aiocsv/aiocsv.py
+++ aiocsv/aiocsv.py
@@ -3,15 +3,17 @@ from collections import OrderedDict
 import asyncio
 
 
-def reader(aiofile, delimeter=',', quotechar='"'):
-    return AioCSVReader(aiofile, delimeter, quotechar)
+def reader(aiofile, delimeter=',', quotechar='"', encoding='ascii'):
+    return AioCSVReader(aiofile, delimeter, quotechar, encoding)
 
 
 class AioCSVReader:
-    def __init__(self, aiofile, delimeter, quotechar):
+    def __init__(self, aiofile, delimeter, quotechar, encoding):
         self._aiofile = aiofile
         self._delimeter = delimeter
-        self._quotechar = quotechar
+        self._quotechar = quotechar.encode(encoding)
+        self._encoding = encoding
+        self.line_num = 0
 
     def __aiter__(self):
         return self
@@ -19,23 +21,26 @@ class AioCSVReader:
     async def __anext__(self):
         line = await self._aiofile.readline()
         if line:
-            return line.strip().replace(self._quotechar.encode('ascii'), b'').decode('ascii').split(self._delimeter)
+            self.line_num += 1
+            return line.strip().replace(self._quotechar, b'').decode(self._encoding).split(self._delimeter)
         else:
             raise StopAsyncIteration
 
+    next = __anext__
+
 
 class AioDictReader(AioCSVReader):
-    def __init__(self, aiofile, delimeter=',', quotechar='"'):
-        super().__init__(aiofile, delimeter, quotechar)
-        self._header = None
+    def __init__(self, aiofile, delimeter=',', quotechar='"', encoding='ascii'):
+        super().__init__(aiofile, delimeter, quotechar, encoding)
+        self.fieldnames = None
 
     async def __anext__(self):
         line = await super().__anext__()
 
-        if self._header is None:
-            self._header = line
-            line_dict = await asyncio.ensure_future(self.__anext__())
+        if self.fieldnames is None:
+            self.fieldnames = line
+            line_dict = await self.__anext__()
         else:
-            line_dict = OrderedDict({k: v for (k, v) in zip(self._header, line)})
+            line_dict = OrderedDict({k: v for (k, v) in zip(self.fieldnames, line)})
 
         return line_dict
